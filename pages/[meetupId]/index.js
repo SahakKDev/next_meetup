93193e2faea722d56040304f5c8230790e953138
Sahak Keshishyan <sahak.keshishyan.dev@gmail.com>
2024-03-10 12:32:11 +0400

    fix ballback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -41,7 +41,7 @@ export async function getStaticPaths() {
   await client.close();
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetups.map((meetup) => ({
       params: {
         meetupId: meetup._id.toString(),
